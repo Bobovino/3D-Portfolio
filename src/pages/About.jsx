2874d58ba3c11195a139764894be1f9dc0290187
Bobovino <rodrigomartineztabernero97@gmail.com>
2024-01-25 14:08:23 +0100

    Tooltips, touchscreen and sorted experiences

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,4 @@
+import { Tooltip } from "react-tooltip";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
@@ -7,8 +8,11 @@ import { CTA } from "../components";
 import { experiences, skills } from "../constants";
 
 import "react-vertical-timeline-component/style.min.css";
+import "react-tooltip/dist/react-tooltip.css";
 
 const About = () => {
+  const reversedExperiences = [...experiences].reverse();
+
   return (
     <section className='max-container'>
       <h1 className='head-text'>
@@ -33,15 +37,26 @@ const About = () => {
 
         <div className='mt-16 flex flex-wrap gap-12'>
           {skills.map((skill) => (
-            <div className='block-container w-20 h-20' key={skill.name}>
-              <div className='btn-back rounded-xl' />
-              <div className='btn-front rounded-xl flex justify-center items-center'>
+            <div
+              className="block-container w-20 h-20"
+              key={skill.name}
+              data-tooltip-id={`tooltip-${skill.name}`}
+            >
+              <div className="btn-back rounded-xl" />
+              <div className="btn-front rounded-xl flex justify-center items-center">
                 <img
                   src={skill.imageUrl}
                   alt={skill.name}
                   className='w-1/2 h-1/2 object-contain'
                 />
               </div>
+              <Tooltip
+                id={`tooltip-${skill.name}`}
+                effect="solid"
+                place="bottom"
+                content={`${skill.name}`}
+                style={{ backgroundColor: "#999" }}
+              />
             </div>
           ))}
         </div>
@@ -58,7 +73,7 @@ const About = () => {
 
         <div className='mt-12 flex'>
           <VerticalTimeline>
-            {experiences.map((experience, index) => (
+            {reversedExperiences.map((experience, index) => (
               <VerticalTimelineElement
                 key={experience.company_name}
                 date={experience.date}

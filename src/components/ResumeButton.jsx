7205b158939207c19dd87b16187c9ee0fd788f33
Bobovino <rodrigomartineztabernero97@gmail.com>
2024-02-07 00:08:17 +0100

    Download resume

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -1,47 +1,17 @@
 import englishResume from '../assets/pdf/English-Resume.pdf';
-import spanishResume from '../assets/pdf/Español-CV.pdf';
-import germanResume from '../assets/pdf/Deutsch-CV.pdf';
-import { useState } from 'react';
-import { FaRegFilePdf, FaCaretDown } from 'react-icons/fa';
-
+import { FaRegFilePdf} from 'react-icons/fa';
+ 
 const ResumeButton = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const resumes = [
-    { name: 'English ', file: englishResume },
-   /*  { name: 'Spanish Resume', file: spanishResume },
-    { name: 'German Resume', file: germanResume }, */
-  ];
-
   return (
-    <div className="relative inline-block text-left">
-      <button
-        onClick={() => setShowDropdown(!showDropdown)}
-        className="flex items-center bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded inline-flex justify-center"
-      >
-        <span>Download Resume</span>
-        <FaCaretDown className="ml-2" />
-      </button>
-      {showDropdown && (
-        <div className="origin-top-right z-20 absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"   style={{ zIndex: 20}}>
-          <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-            {resumes.map((resume, index) => (
-              <a
-                key={index}
-                href={resume.file}
-                download
-                className="flex justify-center block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 z-999"
-                role="menuitem"
-              >
-                {resume.name}
-                <FaRegFilePdf className='ml-2 '/>
-              </a>
-            ))}
-          </div>
-        </div>
-      )}
-    </div>
+    <a
+      href={englishResume}
+      download="Rodrigo_Martinez_Tabernero_Resume_English.pdf"
+      className="flex items-center bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded inline-flex justify-center"
+    >
+      <span>Download Resume</span>
+      <FaRegFilePdf className="ml-2" />
+    </a>
   );
 };
 
-export default ResumeButton;
+export default ResumeButton

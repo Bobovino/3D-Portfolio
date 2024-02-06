7e4d8d6fc9099865b0b643264160765a34815432
Bobovino <rodrigomartineztabernero97@gmail.com>
2024-02-07 00:15:01 +0100

    smol

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
             <p className="blue-gradient_text">RM</p>
         </NavLink>
-      <nav className='flex text-lg gap-4 font-medium items-center'>
+      <nav className='flex text-lg gap-2 sm:gap-4 font-medium items-center'>
           <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
             About
           </NavLink>
--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -6,10 +6,10 @@ const ResumeButton = () => {
     <a
       href={englishResume}
       download="Rodrigo_Martinez_Tabernero_Resume_English.pdf"
-      className="flex items-center bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded inline-flex justify-center"
+      className="flex items-center bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 sm:py-2 sm:px-4 rounded inline-flex justify-center"
     >
       <span>Download Resume</span>
-      <FaRegFilePdf className="ml-2" />
+      <FaRegFilePdf className="ml-1 sm:ml-2" />
     </a>
   );
 };

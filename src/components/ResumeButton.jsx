42ea893751692d5b3c8492c9b27357b62203c646
Bobovino <rodrigomartineztabernero97@gmail.com>
2024-02-07 00:43:41 +0100

    No download for phones

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -1,17 +1,28 @@
 import englishResume from '../assets/pdf/English-Resume.pdf';
-import { FaRegFilePdf} from 'react-icons/fa';
+import { FaRegFilePdf, FaDownload } from 'react-icons/fa';
+
  
 const ResumeButton = () => {
-  return (
+  return <>
     <a
       href={englishResume}
       download="Rodrigo_Martinez_Tabernero_Resume_English.pdf"
-      className="flex items-center bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 sm:py-2 sm:px-4 rounded inline-flex justify-center"
+      className="hidden sm:inline-flex flex items-center bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 sm:py-2 sm:px-4 rounded  justify-center"
     >
       <span>Download Resume</span>
       <FaRegFilePdf className="ml-1 sm:ml-2" />
     </a>
-  );
+    <a
+      href={englishResume}
+      download="Rodrigo_Martinez_Tabernero_Resume_English.pdf"
+      className="sm:hidden flex items-center bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 sm:py-2 sm:px-4 rounded inline-flex justify-center"
+    > 
+      <span>Resume</span>
+      <FaDownload className="ml-1" />
+      <FaRegFilePdf className="ml-1 " />
+    </a>
+    </>
+ 
 };
 
 export default ResumeButton
